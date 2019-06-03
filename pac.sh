07ac746e04296472bc3d35e99e9e7aa4166db775
damang2 <50958605+damang2@users.noreply.github.com>
2019-06-03 18:18:12 +0800

    Add files via upload

---
+++ pac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+pkg install update
+pkg install upgrade
+apt update
+apt upgrade -y
+pkg install php -y
+bash unzip.sh
+
+echo -e "\e[1m\e[31m Subscribe :\e[32m Termux Tricks & Tut"
+echo
+echo -e "\e[1m\e[32m Please Read:\e[32mExit termux and 📝go to star folder open it and open 
+config.php use es file explorer to do this.📒
+👁‍🗨follow the video as your guide👁‍🗨 "
+echo
