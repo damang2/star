044d87f41385a30089c164563f6d05329b8c4e5c
damang2 <50958605+damang2@users.noreply.github.com>
2019-06-03 18:28:43 +0800

    Update pac.sh

--- pac.sh
+++ pac.sh
@@ -9,7 +9,7 @@ bash unzip.sh
 
 echo -e "\e[1m\e[31m Subscribe :\e[32m Termux Tricks & Tut"
 echo
-echo -e "\e[1m\e[32m Please Read:\e[32mExit termux and 📝go to star folder open it and open 
-config.php use es file explorer to do this.📒
+echo -e "\e[1m\e[32m Please Read:\e[32mExit termux and go to star folder
+open it and open config.php use es file explorer to do this.
 👁‍🗨follow the video as your guide👁‍🗨 "
 echo
